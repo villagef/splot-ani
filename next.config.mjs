69570d69c17887016591bb2e5054dd3966b67a5a
villagef <filwyd123@gmail.com>
2024-02-11 15:28:40 +0100

    Home page layout

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
 		remotePatterns: [
 			{
 				protocol: "https",
-				hostname: "handmaderki.pl",
+				hostname: "www.handmaderki.pl",
 				port: "",
 				pathname: "/**",
 			},
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { ProductList } from "@/ui/components/ProductList";
 
 export default function Home() {
 	return (
-		<section className="mx-auto max-w-md p-2 sm:max-w-2xl sm:py-16 md:max-w-4xl lg:max-w-7xl">
+		<section className="mx-auto">
 			<ProductList products={products.data} />
 		</section>
 	);
